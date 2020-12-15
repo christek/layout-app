f6f76edbbae3a2f40e7ea1223a9bea7e6ad6cba4
Christopher Tokar <christopher.tokar@ibm.com>
2020-12-15 15:29:02 +0100

    prettier form command line and prettier applied

--- src/app.js
+++ src/app.js
@@ -10,10 +10,18 @@ const App = () => (
         Clicked:
         <span id="value">0</span>
         times
-        <button type="button" id="increment">+</button>
-        <button type="button" id="decrement">-</button>
-        <button type="button" id="incrementIfOdd">Increment if odd</button>
-        <button type="button" id="incrementAsync">Increment async</button>
+        <button type="button" id="increment">
+          +
+        </button>
+        <button type="button" id="decrement">
+          -
+        </button>
+        <button type="button" id="incrementIfOdd">
+          Increment if odd
+        </button>
+        <button type="button" id="incrementAsync">
+          Increment async
+        </button>
       </p>
     </div>
   </div>
