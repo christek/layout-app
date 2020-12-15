b25454a1ffa8c8c61edcf33491d8cda6561afbb9
Christopher Tokar <christopher.tokar@ibm.com>
2020-12-15 15:23:04 +0100

    lint command line, lint errors fixed

--- src/app.js
+++ src/app.js
@@ -7,11 +7,13 @@ const App = () => (
   <div className={styles.danger}>
     <div>
       <p>
-        Clicked: <span id="value">0</span> times
-        <button id="increment">+</button>
-        <button id="decrement">-</button>
-        <button id="incrementIfOdd">Increment if odd</button>
-        <button id="incrementAsync">Increment async</button>
+        Clicked:
+        <span id="value">0</span>
+        times
+        <button type="button" id="increment">+</button>
+        <button type="button" id="decrement">-</button>
+        <button type="button" id="incrementIfOdd">Increment if odd</button>
+        <button type="button" id="incrementAsync">Increment async</button>
       </p>
     </div>
   </div>
